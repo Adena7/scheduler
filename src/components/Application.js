d31b183fe9a9d2f6278bbb097dc5f05614e5dd5a
Adena7 <abdoullahi.aden@gmail.com>
2022-01-22 18:15:30 -0700

    managing state of our days

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,8 +45,15 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+  }
 
   useEffect(() => {
     axios.get('http://localhost:8001/api/days')
@@ -71,8 +78,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          day={state.day}
           onChange={setDay}
         />
       </nav>
