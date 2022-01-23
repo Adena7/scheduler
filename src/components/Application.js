df6eb495fee898f887d99297bba9eabac97729ba
Adena7 <abdoullahi.aden@gmail.com>
2022-01-22 19:14:07 -0700

    retreiving interview data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,14 +3,15 @@ import axios from "axios";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -19,19 +20,22 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments'))
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers'))
     ])
       .then((all) => {
         setState(prev => ({ ...prev, 
           days: all[0].data,
-          appointments: all[1].data
+          appointments: all[1].data,
+          interviewers: all[2].data
         }));
       })
   }, []);
 
-  const appointmentList = dailyAppointments.map((appointment) => {
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview)
     return (
-      <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />
+      <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={interview} />
     );
   })
   return (
@@ -57,7 +61,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {appointmentList}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
